0561ca23f46c2deff8f27fb30910b932a207e24f
Filippasoderlund <filippa.soderlund@medieinstitutet.se>
2022-12-04 18:15:56 +0100

    eslint och prettier

--- README.md
+++ README.md
@@ -1,40 +1,33 @@
 [![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-c66648af7eb3fe8bc4f294546bfd86ef473780cde1dea487d3c4ff354943c9ae.svg)](https://classroom.github.com/online_ide?assignment_repo_id=9317120&assignment_repo_type=AssignmentRepo)
 
-***************************************************
+---
+
 Få till en sortering som uppdaeras
 
 Få till att bekräftelserutan dyker upp
 
-Få ut summan i varukorg som man ser 
-    -Få in visuell feedback på uppdaterad varukorg
-
-Se till att formulären blir validerade 
-
+Se till att formulären blir validerade
 Fixa så faktura och kort knapp visas vid klick
-    -validera faktura ej kort
+-validera faktura ej kort
 
 Få in rabbater
-    -måndagstabbat har fungerat fungerar ej nu 
-    -fredagsrabatt kolla om den fungerar 
-    -ingen faktura över 800kr få till 
-    -10% på specifik sort köp över 10 munkar
+-måndagstabbat har fungerat fungerar ej nu
+-fredagsrabatt kolla om den fungerar
+-ingen faktura över 800kr få till
+-10% på specifik sort köp över 10 munkar
 
-Innan inläming: 
-    -Få in komentarer till koden 
-    -Validera html och css
-    -Städa i koden
+Innan inläming:
+-Få in komentarer till koden
+-Validera html och css
+-Städa i koden
 
-Lördag:
+söndag:
 Få till formuläret
-Få till rabatter 
+Få till rabatter
 Få till så faktura/kort ändast visas när ifyllt.
 Komentera befintilig kod.
-Få till att sorteringen fungerar med munkarna 
-Hur gör jag med beckräftelse? Samma som faktura/kort, (om knapp tryckt visas bekräftelsen) 
+Få till att sorteringen fungerar med munkarna
+Hur gör jag med beckräftelse? Samma som faktura/kort, (om knapp tryckt visas bekräftelsen)
 Lägg på css
-påbörja validering 
+påbörja validering
 se över npm och eslint osv
-
-
-
-
--- script.js
+++ script.js
@@ -1,79 +1,77 @@
-
-// summa munkar/ filtrering 
+// summa munkar/ filtrering
 
 const donuts = [
   {
-    img: '/style/foto/donut1.png',
-    name: 'Donut 1',
-    price: 40, 
-    amount: 0, 
+    img: "/style/foto/donut1.png",
+    name: "Donut 1",
+    price: 40,
+    amount: 0,
   },
   {
-    img: '/style/foto/donut2.png',
-    name: 'Donut 2',
-    price: 40,  
-    amount: 0, 
+    img: "/style/foto/donut2.png",
+    name: "Donut 2",
+    price: 40,
+    amount: 0,
   },
   {
-    img: '/style/foto/donut3.png',
-    name: 'Donut 3',
-    price: 45,  
-    amount: 0, 
+    img: "/style/foto/donut3.png",
+    name: "Donut 3",
+    price: 45,
+    amount: 0,
   },
   {
-    img: '/style/foto/donut4.png',
-    name: 'Donut 4',
-    price: 55,  
-    amount: 0, 
+    img: "/style/foto/donut4.png",
+    name: "Donut 4",
+    price: 55,
+    amount: 0,
   },
   {
-    img: '/style/foto/donut5.png',
-    name: 'Donut 5',
-    price: 55,  
-    amount: 0, 
+    img: "/style/foto/donut5.png",
+    name: "Donut 5",
+    price: 55,
+    amount: 0,
   },
   {
-    img: '/style/foto/donut6.png',
-    name: 'Donut 6',
-    price: 60,  
-    amount: 0, 
+    img: "/style/foto/donut6.png",
+    name: "Donut 6",
+    price: 60,
+    amount: 0,
   },
   {
-    img: '/style/foto/donut7.png',
-    name: 'Donut 7',
-    price: 70,  
-    amount: 0, 
+    img: "/style/foto/donut7.png",
+    name: "Donut 7",
+    price: 70,
+    amount: 0,
   },
   {
-    img: '/style/foto/donut8.png',
-    name: 'Donut 8',
-    price: 70,  
-    amount: 0, 
+    img: "/style/foto/donut8.png",
+    name: "Donut 8",
+    price: 70,
+    amount: 0,
   },
   {
-    img: '/style/foto/donut9.png',
-    name: 'Donut 9',
-    price: 80,  
-    amount: 0, 
+    img: "/style/foto/donut9.png",
+    name: "Donut 9",
+    price: 80,
+    amount: 0,
   },
   {
-    img: '/style/foto/donut10.png',
-    name: 'Donut 10',
-    price: 90,  
-    amount: 0, 
+    img: "/style/foto/donut10.png",
+    name: "Donut 10",
+    price: 90,
+    amount: 0,
     sortSum: 0,
   },
 ];
 
 //donutsListing = donutHtmlContainer
 
-
-const donutHtmlContainer = document.querySelector('#donutsListing');
-const priceRangeSlider = document.querySelector('#priceRangeSlider');
-const currentRangeValue = document.querySelector('#currentRangeValue');
-const clearCartBtn = document.querySelector('#clearCart');
-const clearFormBtn = document.querySelector('#clearForm');
-const cartHtmlContainer = document.querySelector('#cart');
+const donutHtmlContainer = document.querySelector("#donutsListing");
+const priceRangeSlider = document.querySelector("#priceRangeSlider");
+const currentRangeValue = document.querySelector("#currentRangeValue");
+const clearCartBtn = document.querySelector("#clearCart");
+const clearFormBtn = document.querySelector("#clearForm");
+const cartHtmlContainer = document.querySelector("#cart");
 
 const today = new Date();
 const isFriday = today.getDay() === 5;
@@ -86,24 +84,20 @@ let filteredDonuts = [...donuts];
 let filteredDonutsInPriceRange = [...donuts];
 
 function stupidCustomerMessage() {
-  alert('Du är för långsam på att beställa');
+  alert("Du är för långsam på att beställa");
 }
 
-
 function getPriceMultiplier() {
-  if((isFriday && currentHours >= 15) || (isMonday && currentHours <= 3)) {
+  if ((isFriday && currentHours >= 15) || (isMonday && currentHours <= 3)) {
     return 1.15;
   }
   return 1;
 }
 
-
-
 function printDonuts() {
   donutHtmlContainer.innerHTML = "";
 
   let priceIncrease = 1;
-
 
   for (let i = 0; i < donuts.length; i++) {
     donutHtmlContainer.innerHTML += `
@@ -114,31 +108,26 @@ function printDonuts() {
         Antal <span class="amount">${donuts[i].amount} st</span><br>
         <button class="subtract" data-id="${i}">-</button>
         <button class="add" data-id="${i}">+</button>
-      </article>
+      </article> 
     `;
   }
 
-  document.querySelectorAll('button.add').forEach((btn) => {
-    btn.addEventListener('click', updateDonutAmount);
-  });
-    
-  document.querySelectorAll('button.subtract').forEach((btn) => {
-    btn.addEventListener('click', decreaseDonutAmount);
+  document.querySelectorAll("button.add").forEach((btn) => {
+    btn.addEventListener("click", updateDonutAmount);
   });
 
-  const sum = donuts.reduce(
-    (previousValue, donut) => {
-      return (donut.amount + donut.price) + previousValue;
-    },
-    0
-  );
+  document.querySelectorAll("button.subtract").forEach((btn) => {
+    btn.addEventListener("click", decreaseDonutAmount);
+  });
+
+  const sum = donuts.reduce((previousValue, donut) => {
+    return donut.amount + donut.price + previousValue;
+  }, 0);
 
   printCartDonuts();
-    
-  document.querySelector('#cartSum').innerHTML = sum;
 
+  document.querySelector("#cartSum").innerHTML = sum;
 }
-
 
 function updateDonutAmount(e) {
   const donutClick = e.currentTarget.dataset.id;
@@ -150,77 +139,78 @@ function updateDonutAmount(e) {
 function decreaseDonutAmount(e) {
   const donutClicked = e.currentTarget.dataset.id;
 
-    if (donuts[donutClicked].amount > 0) {
-        donuts[donutClicked].amount -= 1;
-    }
+  if (donuts[donutClicked].amount > 0) {
+    donuts[donutClicked].amount -= 1;
+  }
 
-    printDonuts();
+  printDonuts();
 }
 
-
 function printCartDonuts() {
-  cartHtmlContainer.innerHTML = '';
+  cartHtmlContainer.innerHTML = "";
 
   let sum = 0;
   let orderedDonutAmount = 0;
-  let msg = '';
+  let msg = "";
   let priceIncrease = getPriceMultiplier();
 
-
-  donuts.forEach(donuts => { 
+  donuts.forEach((donuts) => {
     orderedDonutAmount += donuts.amount;
-      if(donuts.amount > 0) { 
-        let donutPrice = donuts.price;
-        if(donuts.amount >= 10) {
-          donutPrice *= 0.9;
-        }
-        const adjustedDonutPrice = donuts.price * priceIncrease;
+    if (donuts.amount > 0) {
+      let donutPrice = donuts.price;
+      if (donuts.amount >= 10) {
+        donutPrice *= 0.9;
+      }
+      const adjustedDonutPrice = donuts.price * priceIncrease;
 
-        sum += donuts.amount * adjustedDonutPrice;
+      sum += donuts.amount * adjustedDonutPrice;
 
-        cartHtmlContainer.innerHTML += `
+      cartHtmlContainer.innerHTML += `
             <article>
-                <span>${donuts.name}</span> | <span>${donuts.amount}</span> | <span>${donuts.amount * adjustedDonutPrice} kr</span> 
+                <span>${donuts.name}</span> | <span>${
+        donuts.amount
+      }</span> | <span>${donuts.amount * adjustedDonutPrice} kr</span> 
             </article>
         `;
-      }   
+    }
   });
 
-  if(sum <= 0) {
+  if (sum <= 0) {
     return;
   }
 
-  if(today.getDay() === 1) {
-      sum += 0.9;
-      msg += '<p>Måndagsrabatt: 10 % på hela köpet';
+  if (today.getDay() === 1) {
+    sum += 0.9;
+    msg += "<p>Måndagsrabatt: 10 % på hela köpet";
   }
 
   cartHtmlContainer.innerHTML += `<p>Total sum: ${sum} kr<p>`;
   cartHtmlContainer.innerHTML += `<div>${msg}</div>`;
 
   if (orderedDonutAmount > 15) {
-    cartHtmlContainer.innerHTML += '<p>Shipping: 0 kr</p>';
+    cartHtmlContainer.innerHTML += "<p>Shipping: 0 kr</p>";
   } else {
-    cartHtmlContainer.innerHTML += `<p>Shipping: ${Math.round(25 + (0.1 * sum))} kr</p>`;
+    cartHtmlContainer.innerHTML += `<p>Shipping: ${Math.round(
+      25 + 0.1 * sum
+    )} kr</p>`;
   }
 }
 
 printDonuts();
 
+clearCartBtn.addEventListener("click", clearCart);
 
-clearCartBtn.addEventListener('click', clearCart);
-
-function clearCart (e){
-  for (let i = 0; i<donuts.length; i++){
+function clearCart(e) {
+  for (let i = 0; i < donuts.length; i++) {
     donuts[i].amount = 0;
   }
 
-  printDonuts(donuts); 
+  printDonuts(donuts);
 }
 
 printDonuts();
 
-//produktfiltrering 
+//produktfiltrering
 
 priceRangeSlider.addEventListener("change", showPrice);
 
@@ -233,28 +223,29 @@ function priceRange() {
   const currentPrice = priceRangeSlider.value;
   currentRangeValue.innerHTML = currentPrice;
 
-  filteredDonutsInPriceRange = filteredDonuts.filter(donut => donut.price <= currentPrice);
+  filteredDonutsInPriceRange = filteredDonuts.filter(
+    (donut) => donut.price <= currentPrice
+  );
   printDonuts();
 }
 
-//kundinfo errorFieldName
+// kundinfo
 
-const errorNameField = document.querySelector('#errorNameField');
+const errorNameField = document.querySelector("#errorNameField");
 
-
-const generateButton = document.querySelector('#customerinfo');
-const nameField = document.querySelector('#name');
-const streatField = document.querySelector('#streat');
-const postnumberField = document.querySelector('#postnumber');
-const cityField = document.querySelector('#city');
-const codeField = document.querySelector('#code');
-const numberField = document.querySelector('#number');
-const emailField = document.querySelector('#email');
-const ssnField = document.querySelector('#ssn');
-const cardField = document.querySelector('#card');
-const dateField = document.querySelector('#date');
-const yearField = document.querySelector('#year');
-const CVCField = document.querySelector('#CVC');
+const generateButton = document.querySelector("#customerinfo");
+const nameField = document.querySelector("#name");
+const streatField = document.querySelector("#streat");
+const postnumberField = document.querySelector("#postnumber");
+const cityField = document.querySelector("#city");
+const codeField = document.querySelector("#code");
+const numberField = document.querySelector("#number");
+const emailField = document.querySelector("#email");
+const ssnField = document.querySelector("#ssn");
+const cardField = document.querySelector("#card");
+const dateField = document.querySelector("#date");
+const yearField = document.querySelector("#year");
+const CVCField = document.querySelector("#CVC");
 
 let nameIsOk = false;
 let streatIsOk = false;
@@ -269,20 +260,18 @@ let dateIsOk = false;
 let yearIsOk = false;
 let CVCIsOk = false;
 
-nameField.addEventListener('change', checkName);
-streatField.addEventListener('change', checkStreat);
-postnumberField.addEventListener('change', checkPostnumber);
-cityField.addEventListener('change', checkcity);
-codeField.addEventListener('change', checkcode);
-numberField.addEventListener('change', checkNumber);
-emailField.addEventListener('change', checkEmail);
-ssnField.addEventListener('change', checkSsn);
-cardField.addEventListener('change', checkCard);
-dateField.addEventListener('change', checkDate);
-yearField.addEventListener('change', checkYear);
-CVCField.addEventListener('change', checkCVC);
-
-
+nameField.addEventListener("change", checkName);
+streatField.addEventListener("change", checkStreat);
+postnumberField.addEventListener("change", checkPostnumber);
+cityField.addEventListener("change", checkcity);
+codeField.addEventListener("change", checkcode);
+numberField.addEventListener("change", checkNumber);
+emailField.addEventListener("change", checkEmail);
+ssnField.addEventListener("change", checkSsn);
+cardField.addEventListener("change", checkCard);
+dateField.addEventListener("change", checkDate);
+yearField.addEventListener("change", checkYear);
+CVCField.addEventListener("change", checkCVC);
 
 /**
  * function validateInput(fname, errorField) {
@@ -302,117 +291,124 @@ validateInput('fname', 'errorField');
  * 
  */
 
- 
 function checkCVC() {
-  CVCIsOk = CVCField.value.indexOf(' ') > -1;
+  CVCIsOk = CVCField.value.indexOf(" ") > -1;
   activateGenerateButton();
 }
 
 function checkYear() {
-  yearIsOk = yearField.value.indexOf(' ') > -1;
+  yearIsOk = yearField.value.indexOf(" ") > -1;
   activateGenerateButton();
 }
 
 function checkDate() {
-  dateIsOk = dateField.value.indexOf(' ') > -1;
+  dateIsOk = dateField.value.indexOf(" ") > -1;
   activateGenerateButton();
 }
 
 function checkCard() {
-  cardIsOk = cardField.value.indexOf(' ') > -1;
+  cardIsOk = cardField.value.indexOf(" ") > -1;
   activateGenerateButton();
 }
 
 function checkSsn() {
-  ssnIsOk = ssnField.value.indexOf(' ') > -1;
+  ssnIsOk = ssnField.value.indexOf(" ") > -1;
   activateGenerateButton();
 }
 
 function checkEmail() {
-  emailIsOk = emailField.value.indexOf(' ') > -1;
+  emailIsOk = emailField.value.indexOf(" ") > -1;
   activateGenerateButton();
 }
 
 function checkNumber() {
-  numberIsOk = numberField.value.indexOf(' ') > -1;
+  numberIsOk = numberField.value.indexOf(" ") > -1;
   activateGenerateButton();
 }
 
 function checkcode() {
-  codeIsOk = codeField.value.indexOf(' ') > -1;
+  codeIsOk = codeField.value.indexOf(" ") > -1;
   activateGenerateButton();
 }
 
 function checkcity() {
-  cityIsOk = cityField.value.indexOf(' ') > -1;
+  cityIsOk = cityField.value.indexOf(" ") > -1;
   activateGenerateButton();
 }
 
 function checkPostnumber() {
-  postnumberIsOk = postnumberField.value.indexOf(' ') > -1;
+  postnumberIsOk = postnumberField.value.indexOf(" ") > -1;
   activateGenerateButton();
 }
 
 function checkStreat() {
-  if (streatField.value.indexOf(' ') > -1) { 
+  if (streatField.value.indexOf(" ") > -1) {
     streatIsOk = true;
-    
   } else {
     streatIsOk = false;
-    
   }
   activateGenerateButton();
 }
 
 function checkName() {
-  if (nameField.value.indexOf(' ') > -1) { 
+  if (nameField.value.indexOf(" ") > -1) {
     nameIsOk = true;
-    
-    
   } else {
     nameIsOk = false;
-    errorNameField.removeAttribute('hidden', '');
+    errorNameField.removeAttribute("hidden", "");
   }
   activateGenerateButton();
 }
 
 function activateGenerateButton() {
-  if(nameIsOk && streatIsOk && postnumberIsOk && cityIsOk && codeIsOk && numberIsOk && emailIsOk && ssnIsOk && CVCIsOk && cardIsOk && dateIsOk && yearIsOk) {
-    generateButton.removeAttribute('disabled');
+  if (
+    nameIsOk &&
+    streatIsOk &&
+    postnumberIsOk &&
+    cityIsOk &&
+    codeIsOk &&
+    numberIsOk &&
+    emailIsOk &&
+    ssnIsOk &&
+    CVCIsOk &&
+    cardIsOk &&
+    dateIsOk &&
+    yearIsOk
+  ) {
+    generateButton.removeAttribute("disabled");
   } else {
-    generateButton.setAttribute('disabled', '');
+    generateButton.setAttribute("disabled", "");
   }
 }
 
+clearFormBtn.addEventListener("click", clearForm);
 
-clearFormBtn.addEventListener('click', clearForm);
-
-function clearForm (e){
-  for (let i = 0; i<donuts.length; i++){
+function clearForm(e) {
+  for (let i = 0; i < donuts.length; i++) {
     donuts[i].amount = 0;
   }
 
-  printDonuts(donuts); 
+  printDonuts(donuts);
 }
 
 //Betalnings metod
 
-const contentBtn1 = document.querySelector('#card');
-const contentBtn2 = document.querySelector('#invoice');
+const contentBtn1 = document.querySelector("#card");
+const contentBtn2 = document.querySelector("#invoice");
 
-contentBtn1.addEventListener('click', showContent1);
-contentBtn2.addEventListener('click', showContent2);
+contentBtn1.addEventListener("click", showContent1);
+contentBtn2.addEventListener("click", showContent2);
 
 function showContent1() {
-  document.querySelector('#payment').classList.add('orange');
-  document.querySelector('#card').classList.add('visible');
-  document.querySelector('#invoice').classList.remove('visible');
+  document.querySelector("#payment").classList.add("orange");
+  document.querySelector("#card").classList.add("visible");
+  document.querySelector("#invoice").classList.remove("visible");
 }
 
 function showContent2() {
-  document.querySelector('#payment').classList.add('yellow');
-  document.querySelector('#card').classList.remove('visible');
-  document.querySelector('#invoice').classList.add('visible');
+  document.querySelector("#payment").classList.add("yellow");
+  document.querySelector("#card").classList.remove("visible");
+  document.querySelector("#invoice").classList.add("visible");
 }
 
 //beställnings bekräftelse
